50d424dcf14f78c76ba349ecb9406906bc9cc7ce
sssssanechka <143980693+sssssanechka@users.noreply.github.com>
2023-09-21 12:44:31 +0700

    Add files via upload

---
+++ lab1/HelloWorld.java
@@ -0,0 +1,26 @@
+package lab1;
+
+import java.util.Scanner;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in) ;
+        System.out.print("Введите число 1:");
+        double ch1 = scanner.nextDouble();
+        System.out.print("Введите число 2:");
+        double ch2 = scanner.nextDouble();
+
+        System.out.println(("Сумма = " + sum(ch1, ch2)));
+    }
+
+    public static double sum(double ch1, double ch2) {
+
+        return ch1 + ch2;
+    }
+
+    public static double sum2(int ch1, int ch2) {
+
+        return ch1 + ch2;
+    }
+}
---
+++ lab1/zadanie1.java
@@ -0,0 +1,7 @@
+package lab1;
+
+public class zadanie1 {
+        public static void main(String[] args) {
+
+        }
+}
