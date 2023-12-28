f5129d822f6efefc1e7647256cb9a6ce91d2f209
sssssanechka <143980693+sssssanechka@users.noreply.github.com>
2023-12-28 14:00:17 +0700

    Update zd1.java

--- zd1.java
+++ zd1.java
@@ -1,49 +1,15 @@
-package lab6;
-
 import java.util.Scanner;
 
-public class zd1 {
-    public static void main(String args[]) {
-        int numberOf5000, numberOf2000, numberOf1000,numberOf500, numberOf200, numberOf100, numberOf50, numberOf10;
+public class FreeFallTime {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Введите высоту h (в метрах): ");
+        double height = input.nextDouble();
 
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Введите сумму: ");
+        double g = 9.81; // ускорение свободного падения в м/с^2
 
-        int amount = sc.nextInt();
+        double time = Math.sqrt((2 * height) / g); // формула для расчета времени свободного падения
 
-        numberOf5000 = amount / 5000;
-        amount %= 5000;
-
-        numberOf2000 = amount / 2000;
-        amount %= 2000;
-
-        numberOf1000 = amount / 1000;
-        amount %= 1000;
-
-        numberOf500 = amount / 500;
-        amount %= 500;
-
-        numberOf200 = amount / 200;
-        amount %= 200;
-
-        numberOf100 = amount / 100;
-        amount %= 100;
-
-        numberOf50 = amount / 50;
-        amount %= 50;
-
-        numberOf10 = amount / 10;
-        amount %= 10;
-
-        System.out.println("Количество 5000 купюр: " + numberOf5000);
-        System.out.println("Количество 2000 купюр: " + numberOf2000);
-        System.out.println("Количество 1000 купюр: " + numberOf1000);
-        System.out.println("Количество 500 купюр: " + numberOf500);
-        System.out.println("Количество 200 купюр: " + numberOf200);
-        System.out.println("Количество 100 купюр: " + numberOf100);
-        System.out.println("Количество 50 купюр: " + numberOf50);
-        System.out.println("Количество 10 купюр: " + numberOf10);
-        System.out.println("Остаток: " + amount);
-
+        System.out.println("Время свободного падения: " + time + " секунд");
     }
 }
