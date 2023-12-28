5bd2966ad8ccabfcb2def38fa29e75a5b9cec807
sssssanechka <143980693+sssssanechka@users.noreply.github.com>
2023-12-28 13:11:32 +0700

    Update HelloWorld.java

--- lab1/HelloWorld.java
+++ lab1/HelloWorld.java
@@ -3,6 +3,9 @@ package lab1;
 import java.util.Scanner;
 
 public class HelloWorld {
+    public helloworld() {
+    }
+    
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in) ;
